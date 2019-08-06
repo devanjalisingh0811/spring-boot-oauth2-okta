53901da60a22d65d23986e005daf9f55762ca9ba
SINGH Devanjali <g522140@mph.morpho.com>
2019-08-06 18:38:46 +0530

    adding project

---
+++ oauth-okta-client/src/main/java/com/demo/oauth2/oauthoktaclient/OauthOktaClientApplication.java
@@ -0,0 +1,13 @@
+package com.demo.oauth2.oauthoktaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthOktaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthOktaClientApplication.class, args);
+	}
+
+}
---
+++ spring-boot-oauth-client/src/main/java/com/demo/oauth2/springbootoauthclient/SpringBootOauthClientApplication.java
@@ -0,0 +1,13 @@
+package com.demo.oauth2.springbootoauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauthClientApplication.class, args);
+	}
+
+}
